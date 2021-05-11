777462e944402e373c0db2f8bba07cedd95742b4
Vrinda Vyas <vrindavyas10@gmail.com>
2021-05-12 02:00:17 +0530

    Template literals

---
+++ Template Literals.js
@@ -0,0 +1,14 @@
+
+const normal_name ="vyas"
+const greet_normally = "My name is " + normal_name
+
+console.log(greet_normally)
+document.write(greet_normally)
+//using template literals
+const name ="vrinda"
+const age =20
+
+const greeting = `My name is ${name} and my age is ${age}`
+console.log(greeting)
+
+document.write(greeting)
