65b1dbc3c2f9be6e230e8434bf031fc225abd930
Vrinda Vyas <vrindavyas10@gmail.com>
2021-07-16 13:20:37 +0530

    truthy-falsy values

---
+++ Truthy &Falsy_Values.js
@@ -0,0 +1,9 @@
+//const name = " "
+const name = 'vrinda'
+//"",'',false,undefined,null,o are falsy values
+if (name) {
+    console.log(`${name} is my name`)
+}
+else {
+    console.log(`Not me`)
+}
