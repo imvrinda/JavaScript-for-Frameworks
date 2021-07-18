598f256f399a56b6a798f0fbdbc5ea7992cb3586
Vrinda Vyas <vrindavyas10@gmail.com>
2021-07-18 20:36:28 +0530

    Ternary

---
+++ Ternary.js
@@ -0,0 +1,11 @@
+const token = "shcisdacj;osad"
+
+// let user = false
+// if (token) {
+//     user = true
+// }
+// else {
+//     user = false
+// }
+const user = token ? true : false
+console.log(user)
