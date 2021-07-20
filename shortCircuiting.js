300e76fa95b7244d8a71c4787350c6e3c1d764ad
Vrinda Vyas <vrindavyas10@gmail.com>
2021-07-20 14:23:08 +0530

    shortcircuit

---
+++ shortCircuiting.js
@@ -0,0 +1,8 @@
+//console.log("hello")
+const name = "hello"
+const username = name && "User" //User
+
+//const username = name && "User" //hello
+console.log(username)
+
+//can use OR (||) operator also
